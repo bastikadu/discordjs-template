8ab5b94d41cf47b41d94b6be33d75f6300b303c8
bastikadu <87536019+bastikadu@users.noreply.github.com>
2021-08-31 12:15:28 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    // What do you want the bot's prefix to be?
+    prefix: '',// Enter your prefix bewteen the ''. You cannot add SPACE.
+
+    // Who will be the bots author? This will appear as the game from the bot.
+    author: ''// Enter the name you go by on the internetbewteen the ''. You can add SPACE.
+
+}
